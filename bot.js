6c0edf8b8b4d8a9e5f126bdb5064e1c1952f26dc
volfram2008-cmd <volfram2008@gmail.com>
2026-01-04 00:43:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const { Telegraf } = require("telegraf");
-const bot = new Telegraf(process.env.TELEGTAM_TOKEN);
+const bot = new Telegraf(process.env.TELEGTAM_TOKEN= 8015925618:AAHmpwrfU2YY46dSuGswkMAoL15fwAxlu-U);
 
 let nswfYaoiImg = require("./commands/nswf/yaoi-img");
 let nswfTrapImg = require("./commands/nswf/trap-img");
